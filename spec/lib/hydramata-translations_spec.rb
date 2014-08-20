31937424932b20868375f6b4152af721723f231c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-20 14:52:20 -0400

    Removing derelicts of code for Engines

--- spec/lib/hydramata-translations_spec.rb
+++ spec/lib/hydramata-translations_spec.rb
@@ -3,12 +3,5 @@ require 'hydramata-translations'
 
 module Hydramata
   describe Translations do
-    it 'has a .table_name_prefix to conform to Rails::Engine' do
-      expect(described_class.table_name_prefix).to eq('hydramata_translations_')
-    end
-
-    it 'has a .use_relative_model_naming? to conform to Rails::Engine' do
-      expect(described_class.use_relative_model_naming?).to eq(true)
-    end
   end
 end
