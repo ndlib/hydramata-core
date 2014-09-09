b0be281c3934471cd2668849be6206bbae2b5774
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-09-09 14:09:06 -0400

    Enabling configuration injection points
    By following the pattern established in Hyramata::Works, modules can
    be mixed into the Hydramata::Configuration class exposing additional
    configuration options.
    
    The sequence of load for Rails, however, makes this complicated.
    
    The initializers are loaded (i.e. `Hydramata.configure {|config| }`)
    before the ActiveSupport.run_load_hooks are called. This means the
    configuration must be captured and resolved after the load_hooks are
    called.

--- lib/hydramata/core.rb
+++ lib/hydramata/core.rb
@@ -20,9 +20,24 @@ module Hydramata
 
   # @see Configuration
   # @see .configuration
-  def configure
-    yield(configuration)
+  def configure(&block)
+    @configuration_block = block
+
+    # The Rails load sequence means that some of the configured Targets may
+    # not be loaded; As such I am not calling configure! instead relying on
+    # Hydra::RemoteIdentifier::Railtie to handle the configure! call
+    configure! unless defined?(Rails)
   end
+  attr_accessor :configuration
+
+  def configure!
+    if @configuration_block.respond_to?(:call)
+      self.configuration ||= Configuration.new
+      @configuration_block.call(configuration)
+      @configuration_block = nil
+    end
+  end
+  private :configure!
 
   module Core
   end
--- lib/hydramata/core/railtie.rb
+++ lib/hydramata/core/railtie.rb
@@ -4,6 +4,11 @@ module Hydramata
   module Core
     class Railtie < Rails::Railtie
       config.eager_load_namespaces << Hydramata::Core
+      config.to_prepare do
+        # Because I want allow for other components to plug into Hydramata's
+        # configuration. Maybe there is a better way to do this.
+        Hydramata.send(:configure!)
+      end
     end
   end
 end
